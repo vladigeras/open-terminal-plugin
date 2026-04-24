cba5d481b9c34924ffd127f6910027d48dcc5d78
Vladislav Gerasimov <vladi6eras@gmail.com>
2026-04-24 19:01:55 +0300

    Fix tool icon

---
+++ src/main/kotlin/com/vladigeras/openterminal/Icons.kt
@@ -0,0 +1,8 @@
+package com.vladigeras.openterminal
+
+import com.intellij.openapi.util.IconLoader
+
+object Icons {
+    @JvmField
+    val TOOL = IconLoader.getIcon("/icons/tool.svg", javaClass)
+}
